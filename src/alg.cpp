dfaca76457741c1e35ef08a1a85901b17684c804
TiunovaYana <99291404+TiunovaYana@users.noreply.github.com>
2022-02-25 16:23:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,11 +3,11 @@
 int gcd(int a, int b) {
     if (a == b) {
         return a;
-    }
-    else {
-        if (a > b)
-            return gcd(a - b, b);
-        else 
-            return gcd(a, b - a);
+    } else {
+          if (a > b) {
+              return gcd(a - b, b);
+          } else {
+                return gcd(a, b - a);
+            }
     }
 }
